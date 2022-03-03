f9865b9a5615bab9eb6f8aef100835ee1dbc0d7c
it <alizaden.1379@gmail.com>
2022-03-03 23:56:03 +0330

    Comments for singleton added

--- AP2020-hw2/src/market/Market.java
+++ AP2020-hw2/src/market/Market.java
@@ -2,6 +2,12 @@ package market;
 
 public class Market {
 
+    /**
+     * For market class, we will use Singleton design pattern.
+     * Singleton is used when you want to have only one instance of a class in your entire program
+     * In this exercise, all objects should use the instance of Market, so we need only one instance of
+     * Market in our entire program. Hence , we make it Singleton.
+     */
     private static Market market;
 
     public static Market getInstance(){
