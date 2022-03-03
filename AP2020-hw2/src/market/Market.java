1453662bea4c3b5466ad9e17204976f9edc8f24d
it <alizaden.1379@gmail.com>
2022-03-03 23:48:41 +0330

    Making Market singelton

--- AP2020-hw2/src/market/Market.java
+++ AP2020-hw2/src/market/Market.java
@@ -1,4 +1,16 @@
 package market;
 
 public class Market {
+
+    private static Market market;
+
+    public static Market getInstance(){
+        if(market==null)
+            market=new Market();
+        return market;
+    }
+
+    private Market(){
+
+    }
 }
