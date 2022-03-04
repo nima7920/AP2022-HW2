8be63b96f0415c0f3640d72ab6d01895c06c556c
it <alizaden.1379@gmail.com>
2022-03-04 10:15:16 +0330

    Product attributes added

--- AP2020-hw2/src/items/Product.java
+++ AP2020-hw2/src/items/Product.java
@@ -1,4 +1,15 @@
 package items;
 
 public class Product {
+    private String name;
+    private int id;
+
+    public Product(String name,int id){
+        if(name.length()<1 || name.length()>80){
+            System.out.println("Product name is invalid");
+            return;
+        }
+        this.name=name;
+        this.id=id;
+    }
 }
