af66f500e3e8fdd721463d2c6c9470b312c7b9f9
it <alizaden.1379@gmail.com>
2022-03-08 17:33:30 +0330

    Test for Good added

--- AP2020-hw2/test/items/GoodTest.java
+++ AP2020-hw2/test/items/GoodTest.java
@@ -1,9 +1,18 @@
 package items;
 
+import org.junit.Test;
+import users.User;
+
 import static org.junit.Assert.*;
 
 public class GoodTest {
 
-
+    @Test
+    public void goodTest() {
+        Good good=new Good("banana",new User("Nima",99));
+        assertTrue(good instanceof Product);
+        assertEquals("banana",good.getName());
+        assertEquals("Nima",good.getUser().getName());
+    }
 
 }
